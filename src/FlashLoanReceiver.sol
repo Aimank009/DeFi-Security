3ffc820a5bef9c551d81ac20db1696a218b537fa
Aiman Khan <aiman_k@bt.iitr.ac.in>
2026-01-19 03:40:36 +0530

    Complete LP + Oracle and Flash Loan

---
+++ src/FlashLoanReceiver.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./interfaces/IFlashLoanReceiver.sol";
+import "./interfaces/ISecureVault.sol";
+
+contract FlashLoanReceiver is IFlashLoanReceiver {
+    address public vault;
+    address public owner;
+
+    constructor(address _vault) {
+        vault = _vault;
+        owner = msg.sender;
+    }
+    receive() external payable {}
+    function requestFlashLoan(uint256 _amount) public {
+        ISecureVault(vault).flashLoan(_amount, address(this), "");
+    }
+
+    function executeOperation(
+        uint256 _amount,
+        uint256 _fee,
+        address _initiator,
+        bytes calldata _data
+    ) external override returns (bool) {
+        // Do something with the borrowed ETH here
+
+        (bool success, ) = vault.call{value: _amount + _fee}("");
+        require(success, "Repay failed");
+
+        return true;
+    }
+}
